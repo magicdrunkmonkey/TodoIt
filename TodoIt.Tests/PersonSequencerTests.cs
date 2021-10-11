99bdca3ca837b51f1f11625e01f10eb6d05e4690
George Sydby <george.sydby@gmail.com>
2021-10-11 16:44:58 +0200

    PersonSequencer and testing done.

---
+++ TodoIt.Tests/PersonSequencerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIt.Data;
+
+namespace TodoIt.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void IncrementPersonId()
+        {
+            //Arrange
+            int personId = 0;
+            int expectedPersonId = 1;
+
+            //Act
+            int actualPersonId = PersonSequencer.NextPersonId(personId);
+
+            //Assert
+            Assert.Equal(expectedPersonId, actualPersonId);
+        }
+
+        [Fact]
+        public void ResetPersonId()
+        {
+            //Arrange
+            int personId = 135;
+            int expectedPersonId = 0;
+
+            //Act
+            int actualPersonId = PersonSequencer.Reset(personId);
+
+            //Assert
+            Assert.Equal(expectedPersonId, actualPersonId);
+        }
+    }
+}
---
+++ TodoIt/Data/PersonSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+    public class PersonSequencer
+    {
+        private static int personId;
+
+        //Add a static method called nextPersonId that increment and return the next 
+        //personId value.
+        public static int NextPersonId(int personId)
+        {
+            return ++personId;
+        }
+
+        //Add a static method called reset() that sets the personId variable to 0.
+        public static int Reset(int personId)
+        {
+            return personId = 0;
+        }
+    }
+}

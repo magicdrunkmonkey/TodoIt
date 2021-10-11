aa72d9a2b93f33e5246e154ec79844d91dd9accf
George Sydby <george.sydby@gmail.com>
2021-10-11 22:56:25 +0200

    TodoSequencer and TodoSequencerTests - Done

---
+++ TodoIt.Tests/TodoSequencerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIt.Data;
+
+namespace TodoIt.Tests
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void IncrementTodoId()
+        {
+            //Arrange            
+            int expectedTodoId = 5;                               //Initialize expected value
+            int actualTodoId = 0;                                 //Initialize start value
+
+            //Act
+            for (int i = 0; i < expectedTodoId; i++)                //Increment amount equl to expected value
+            {
+                actualTodoId = TodoSequencer.NextTodoId();          //Increment the todoId by one                
+            }
+
+            //Assert
+            Assert.Equal(expectedTodoId, actualTodoId);
+        }
+
+        [Fact]
+        public void ResetTodoId()
+        {
+            //Arrange, initialize values
+            int expectedTodoId = 0;
+            int actualTodoId = 135;
+
+            //Act            
+            actualTodoId = TodoSequencer.Reset();           //Reset TodoId to zero.
+
+            //Assert
+            Assert.Equal(expectedTodoId, actualTodoId);
+        }
+    }
+}
--- TodoIt/Data/TodoSequencer.cs
+++ TodoIt/Data/TodoSequencer.cs
@@ -4,16 +4,21 @@ using System.Text;
 
 namespace TodoIt.Data
 {
-    class TodoSequencer
+    public class TodoSequencer
     {
         //Create a new class TodoSequencer in Data folder that have the same behaviour as 
         //PersonSequencer but different method names.
 
         private static int todoId;
 
-        public static int NextPersonId()
+        public static int NextTodoId()
         {
             return ++todoId;
+        }
+
+        public static int Reset()
+        {
+            return todoId = 0;
         }
     }
 }

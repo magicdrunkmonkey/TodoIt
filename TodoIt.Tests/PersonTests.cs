08d0d86f664d5944a1c6266b626b57538c0e8ee0
George Sydby <george.sydby@gmail.com>
2021-10-11 13:05:40 +0200

    Vet inte vad jag gör

---
+++ TodoIt.Tests/PersonTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using TodoIt.;
+
+namespace TodoIt.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void ConstructorPerson()
+        {
+            //Arrange
+            
+
+            //Act
+
+
+            //Assert
+
+        }
+    }
+}
---
+++ TodoIt/Model/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Model
+{
+    class Person
+    {
+        
+        //Fields
+        private readonly int personId;        
+        private string firstName;
+        private string lastName;
+
+        //Getters and setters
+        readonly int id;
+        readonly string fName;
+        readonly string lName;
+        public int Id 
+        { 
+            get { return id; }
+            set { }
+        }        
+        public string FirstName 
+        { 
+            get { return fName; }
+            set {
+                if (value.Equals("") || value.Equals(null))
+                {
+                    throw new ArgumentException("Not a valid name.");
+                }
+                firstName = value;
+            } 
+        }                
+        public string LastName 
+        {
+            get { return lName; }
+            set {
+                if (value.Equals("") || value.Equals(null))
+                {
+                    throw new ArgumentException("Not a valid name.");
+                }
+                lastName = value;
+            }
+        }
+
+        //Constructors
+        public Person()
+        {
+            this.personId = Id;
+            this.firstName = FirstName;
+            this.lastName = LastName;
+        }
+    }
+}

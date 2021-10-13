cf8c33ab66f67b144c5197e21209c7de70bcfd9d
George Sydby <george.sydby@gmail.com>
2021-10-13 14:59:25 +0200

    Testing method FindByAssignee done.

--- TodoIt.Tests/TodoItemsTests.cs
+++ TodoIt.Tests/TodoItemsTests.cs
@@ -104,14 +104,14 @@ namespace TodoIt.Tests
         public void TestFindById()
         {
             //Prepare
-            TodoSequencer.Reset();
-            string expectedDescription1 = "Kalle Anka var glad i duschen.";
-            string expectedDescription2 = "Musse Pigg spelade boll med Jan Långben.";
-            string expectedDescription3 = "Jan Långben fångade boll med Musse Pigg.";
+            TodoSequencer.Reset();            
 
             //Arrange
             TodoItems todoFindById = new TodoItems();
             todoFindById.Clear();
+            string expectedDescription1 = "Kalle Anka var glad i duschen.";
+            string expectedDescription2 = "Musse Pigg spelade boll med Jan Långben.";
+            string expectedDescription3 = "Jan Långben fångade boll med Musse Pigg.";
 
             //Act            
             Todo item1 = todoFindById.NewTodo(expectedDescription1);
@@ -126,6 +126,79 @@ namespace TodoIt.Tests
             //Cleanup
             TodoSequencer.Reset();
             todoFindById.Clear();
+        }
+
+        [Fact]
+        public void TestFindByDoneStatus()
+        {
+            //Prepare
+            TodoSequencer.Reset();
+
+            //Arrange
+            TodoItems todoFindByDoneStatus = new TodoItems();
+            todoFindByDoneStatus.Clear();
+            string expectedDescription1 = "Kalle Anka var glad i duschen.";
+            string expectedDescription2 = "Musse Pigg spelade boll med Jan Långben.";
+            string expectedDescription3 = "Jan Långben fångade boll med Musse Pigg.";
+
+            Todo item1 = todoFindByDoneStatus.NewTodo(expectedDescription1);
+            Todo item2 = todoFindByDoneStatus.NewTodo(expectedDescription2);
+            Todo item3 = todoFindByDoneStatus.NewTodo(expectedDescription3);
+
+            item1.Done = true;
+            item2.Done = false;
+            item3.Done = true;
+
+            //Act            
+            Todo[] foundDone = new Todo[3];
+            foundDone = todoFindByDoneStatus.FindByDoneStatus(true);
+
+            //Assert
+            Assert.Equal(2, foundDone.Length);
+
+            //Cleanup
+            TodoSequencer.Reset();
+            todoFindByDoneStatus.Clear();
+        }
+
+        [Fact]
+        public void TestFindByAssignee()
+        {
+            //Prepare
+            TodoSequencer.Reset();
+
+            //Arrange
+            TodoItems todoFindByAssignee = new TodoItems();
+            todoFindByAssignee.Clear();
+            string expectedDescription1 = "Kalle Anka var glad i duschen.";
+            string expectedDescription2 = "Musse Pigg spelade boll med Jan Långben.";
+            string expectedDescription3 = "Jan Långben fångade boll med Musse Pigg.";
+
+            Todo item1 = todoFindByAssignee.NewTodo(expectedDescription1);
+            Todo item2 = todoFindByAssignee.NewTodo(expectedDescription2);
+            Todo item3 = todoFindByAssignee.NewTodo(expectedDescription3);
+
+            People peopleFindByAssignee = new People();
+            peopleFindByAssignee.Clear();
+            Person person1 = peopleFindByAssignee.NewPerson("Lucky", "Luke");
+            Person person2 = peopleFindByAssignee.NewPerson("Alfons", "Åberg");
+
+            item1.Assignee = person1;
+            item2.Assignee = person2;
+            item3.Assignee = person1;
+
+            //Act            
+            Todo[] foundAssignees = new Todo[3];
+            foundAssignees = todoFindByAssignee.FindByAssignee(person1);
+
+            //Assert
+            Assert.Equal(2, foundAssignees.Length);
+
+            //Cleanup
+            TodoSequencer.Reset();
+            PersonSequencer.Reset();
+            todoFindByAssignee.Clear();
+            peopleFindByAssignee.Clear();
         }
     }
 }
